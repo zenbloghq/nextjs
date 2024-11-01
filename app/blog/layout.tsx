3788a61482064504bbc336ec74aaf57332b853d0
Jordi Enric <jordi.err@gmail.com>
2024-11-01 13:56:27 +0100

    wip

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -6,8 +6,11 @@ export default function BlogLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="max-w-4xl mx-auto">
-      <nav className="p-4 border-b border-gray-200">
+    <div className="max-w-5xl mx-auto">
+      <nav className="p-4 border-b border-gray-200 flex justify-between items-center">
+        <Link className="text-lg font-semibold" href="/">
+          Zenblog Next.js
+        </Link>
         <Link href="/">Home</Link>
       </nav>
       {children}
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,34 @@
 /* eslint-disable @next/next/no-img-element */
 import { cms } from "@/lib/cms";
+import { formatDate } from "@/lib/dates";
 import Link from "next/link";
+
+function PostPublishedAt({ publishedAt }: { publishedAt: string }) {
+  return (
+    <time className="text-sm font-medium text-slate-500">
+      {formatDate(publishedAt)}
+    </time>
+  );
+}
+
+function PostCategory({
+  category,
+}: {
+  category: { slug: string; name: string };
+}) {
+  return (
+    <Link
+      href={`/blog/categories/${category.slug}`}
+      className="text-orange-500 font-medium hover:underline"
+    >
+      {category.name}
+    </Link>
+  );
+}
+
+function PostDescription({ description }: { description: string }) {
+  return <p className="text-slate-600 text-balance">{description}</p>;
+}
 
 export default async function BlogPage() {
   let posts;
@@ -14,22 +42,90 @@ export default async function BlogPage() {
     return <div>Error trying to fetch posts.</div>;
   }
 
+  const { data: categories } = await cms.categories.list();
+  const lastPost = posts.data[posts.data.length - 1];
+  const postsWithoutLast = posts.data.slice(0, -1);
+
   return (
-    <div className="p-4">
-      <h1 className="text-3xl font-bold my-8">Blog</h1>
-      <div className="grid grid-cols-2 gap-8">
-        {posts.data.map((post) => (
-          <Link href={`/blog/${post.slug}`} key={post.slug}>
-            <img
-              height="230"
-              width={"300"}
-              className="rounded-lg object-cover w-full max-h-[230px]"
-              src={post.cover_image}
-              alt={post.title}
-            />
-            <h2 className="text-2xl font-bold my-2">{post.title}</h2>
-            <p>{post.excerpt}</p>
+    <div className="p-4 space-y-12">
+      <div>
+        <h1 className="md:text-3xl text-2xl font-bold mt-12">Blog</h1>
+        <p className="text-slate-500 text-lg mt-2">
+          Stay up to date with the latest news and updates from the team!
+        </p>
+      </div>
+      <article className="md:flex gap-8">
+        <Link className="w-full" href={`/blog/${lastPost.slug}`}>
+          <img
+            loading="lazy"
+            width={800}
+            height={400}
+            className="rounded-lg object-cover w-full max-h-[360px]"
+            src={lastPost.cover_image}
+            alt={lastPost.title}
+          />
+        </Link>
+        <div className="md:max-w-xs flex flex-col gap-2 py-2">
+          <div className="flex justify-between items-center">
+            {lastPost.category?.slug ? (
+              <PostCategory category={lastPost.category} />
+            ) : null}
+            <PostPublishedAt publishedAt={lastPost.published_at} />
+          </div>
+          <Link href={`/blog/${lastPost.slug}`}>
+            <div className="space-y-2 text-center md:text-left">
+              <h2 className="text-3xl font-bold">{lastPost.title}</h2>
+              <PostDescription description={lastPost.excerpt || ""} />
+            </div>
           </Link>
+        </div>
+      </article>
+      <hr />
+      <section className="flex flex-col gap-1 items-center">
+        <h2 className="text-sm font-semibold text-slate-500 tracking-wider">
+          CATEGORIES
+        </h2>
+        <ul className="flex gap-1 justify-center">
+          {categories.map((category) => (
+            <li
+              key={category.slug}
+              className="text-xl font-medium text-slate-800 hover:text-orange-600"
+            >
+              <Link
+                className="px-3 py-1 rounded-full hover:bg-orange-50"
+                href={`/blog/categories/${category.slug}`}
+              >
+                {category.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <div className="grid md:grid-cols-2 gap-12">
+        {postsWithoutLast.map((post) => (
+          <article key={post.slug}>
+            <Link href={`/blog/${post.slug}`}>
+              <img
+                height="230"
+                width={"300"}
+                className="rounded-lg object-cover w-full max-h-[230px]"
+                src={post.cover_image}
+                alt={post.title}
+              />
+            </Link>
+            <div className="mt-2">
+              <div className="flex justify-between items-center">
+                {post.category ? (
+                  <PostCategory category={post.category} />
+                ) : null}
+                <PostPublishedAt publishedAt={post.published_at} />
+              </div>
+              <Link href={`/blog/${post.slug}`}>
+                <h2 className="md:text-2xl text-xl font-bold">{post.title}</h2>
+                <PostDescription description={post.excerpt || ""} />
+              </Link>
+            </div>
+          </article>
         ))}
       </div>
     </div>
